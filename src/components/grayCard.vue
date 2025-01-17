90977cb2e51b7e5bf500b2f39a3e7a6dc764301e
Promise Okeke <ugobright31@gmail.com>
2025-01-18 00:37:30 +0100

    small update

---
+++ src/components/grayCard.vue
@@ -0,0 +1,7 @@
+<script setup>
+</script>
+
+<template>
+    <div class="gray-bg">
+    </div>
+</template>
--- src/views/cardContainer.vue
+++ src/views/cardContainer.vue
@@ -1,12 +1,28 @@
 <script setup>
     import Card from '@/components/Card.vue';
-
+    import grayCard from '@/components/grayCard.vue';
 </script>
 
 <template>
-    <div class="container d-flex justify-content-between">
-        <Card/>
-        <Card/>
-        <Card/>
+    <div class="card-container d-flex flex-column justify-content-between">
+        <div class="row-1 row potfolio-card-container">
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+        </div>
+
+        <div class="row-2  d-flex flex-row justify-content-between">
+            <grayCard/>
+            <grayCard/>
+            <grayCard/>
+        </div>
+
+        <div class="row-1 row potfolio-card-container">
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+        </div>
     </div>
 </template>
