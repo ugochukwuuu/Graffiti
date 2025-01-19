bb25b48a2de00fc8bed2e6eecca2cbbf1961f17d
Promise Okeke <ugobright31@gmail.com>
2025-01-20 00:34:44 +0100

    img rendering fix 1

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -11,12 +11,16 @@
     onMounted(()=>{
         console.log(card)
     })
+
+    // const resolveImgPath = (img) => require(`@/assets/card-banners/${img}`);
+
 </script>
 
 <template>
     <div class="card d-flex flex-column">
         <div class="row-1">
             <img class="img" :src="card.img" alt="">
+            <!-- <img :src="resolveImgPath(card.img)" :alt="card.title" /> -->
         </div>
         <div class="row-2 d-flex flex-row align-items-center justify-content-between">
             <h2 class="title">{{ card.title }}</h2>
